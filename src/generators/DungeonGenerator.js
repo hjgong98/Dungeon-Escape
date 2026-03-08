1deecd53a5435c26998f9508c8505712b294826d
hjgong98 <hjgong@ucsc.edu>
2026-03-08 08:30:59 +0000

    added dungeon generator file

---
+++ src/generators/DungeonGenerator.js
@@ -0,0 +1,12 @@
+// DungeonGenerator.js
+// procedurally generates a random dungeon for Dungeons.js
+
+class DungeonGenerator {
+  constructor() {
+    this.floors = [];
+  }
+
+  generateDungeon(minFLoors = 1, maxFloors = 4) {
+    // pick a random number of floors
+  }
+}
